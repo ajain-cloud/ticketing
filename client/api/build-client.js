47051ef6f0bb5f957fe4335f24f8fff69be6d8df
ajain-cloud <aj.turnoutnow@gmail.com>
2025-09-11 19:22:20 +0530

    update base url for server

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,23 +4,23 @@ export default function buildClient({ req }) {
   if (typeof window === 'undefined') {
     // we are on the server
     // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-    return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      headers: req.headers,
-    });
-  } else {
-    // we are on client
-    // requests should be made to base url of ''
     // return axios.create({
-    //   baseURL: '/',
+    //   baseURL:
+    //     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+    //   headers: req.headers,
     // });
 
-    // we are on client
+    // we are on server
     // requests should be made to base url of purchased domain
     // for cluster running in production
     return axios.create({
       baseURL: 'http://www.ticketing-app-production.site/',
+    });
+  } else {
+    // we are on client
+    // requests should be made to base url of ''
+    return axios.create({
+      baseURL: '/',
     });
   }
 }
